f79361118eac0f12f513cefa2bbf0f5e8cdec660
Chan-GN <ocg0229@gmail.com>
2024-10-03 22:06:28 +0900

    COMMENT: DuplicateEmailException 클래스 주석 수정
    - '중복된 로그인 ID' 표현을 '중복된 이메일'로 수정
    - 클래스의 목적을 더 정확히 반영하도록 주석 개선

--- src/main/java/org/example/dotoli/config/error/exception/DuplicateEmailException.java
+++ src/main/java/org/example/dotoli/config/error/exception/DuplicateEmailException.java
@@ -3,7 +3,7 @@ package org.example.dotoli.config.error.exception;
 import org.example.dotoli.config.error.ErrorCode;
 
 /**
- * 중복된 로그인 ID 예외를 표현하는 클래스
+ * 중복된 이메일 예외를 표현하는 클래스
  */
 public class DuplicateEmailException extends BusinessBaseException {
 
