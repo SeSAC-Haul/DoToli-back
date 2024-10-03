7a25ed07cb69e56d7968ce6e90a8dc5eb4934c56
Chan-GN <ocg0229@gmail.com>
2024-10-03 20:05:42 +0900

    FEAT: 인증 서비스 구현
    - AuthenticationService 클래스 추가
      - 회원가입 로직 구현 (saveMember 메소드)
      - 로그인 인증 로직 구현 (authenticate 메소드)
      - 이메일 중복 체크 로직 구현 (checkDuplicateEmail 메소드)
    - 비밀번호 암호화 및 인증 관리자 활용
    - @Transactional 어노테이션을 통한 트랜잭션 관리

---
+++ src/main/java/org/example/dotoli/service/AuthenticationService.java
@@ -0,0 +1,67 @@
+package org.example.dotoli.service;
+
+import org.example.dotoli.config.error.exception.DuplicateEmailException;
+import org.example.dotoli.domain.Member;
+import org.example.dotoli.dto.auth.SignInRequestDto;
+import org.example.dotoli.dto.auth.SignUpRequestDto;
+import org.example.dotoli.repository.MemberRepository;
+import org.example.dotoli.security.userdetails.CustomUserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 인증 관련 비즈니스 로직을 처리하는 서비스 클래스
+ */
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthenticationService {
+
+	private final MemberRepository memberRepository;
+
+	private final PasswordEncoder passwordEncoder;
+
+	private final AuthenticationManager authenticationManager;
+
+	/**
+	 * 회원가입
+	 */
+	@Transactional
+	public Long saveMember(SignUpRequestDto dto) {
+		checkDuplicateEmail(dto.getEmail());
+
+		Member member = Member.createNew(
+				dto.getEmail(), passwordEncoder.encode(dto.getPassword()), dto.getNickname()
+		);
+
+		return memberRepository.save(member).getId();
+	}
+
+	/**
+	 * 로그인
+	 */
+	public CustomUserDetails authenticate(SignInRequestDto dto) {
+		Authentication authenticate = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword())
+		);
+
+		return (CustomUserDetails)authenticate.getPrincipal();
+	}
+
+	/**
+	 * 아이디 중복 체크
+	 */
+	private void checkDuplicateEmail(String email) {
+		memberRepository.findByEmail(email)
+				.ifPresent(m -> {
+					throw new DuplicateEmailException();
+				});
+	}
+
+}
