3be81d157284fbd6e1ae842affb341149df29ca3
liimjiin <limiary27@gmail.com>
2024-10-07 18:16:57 +0900

    COMMENT: SecurityConfig 내부 주석 추가

--- src/main/java/org/example/dotoli/config/security/SecurityConfig.java
+++ src/main/java/org/example/dotoli/config/security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+		configuration.setAllowedOrigins(List.of("http://localhost:5173")); // 프론트엔드 URL
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
