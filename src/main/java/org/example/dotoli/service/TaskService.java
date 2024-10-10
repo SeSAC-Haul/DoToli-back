e993b9e2e1635a95e7e74e0cb9993d40f9ed871f
liimjiin <limiary27@gmail.com>
2024-10-10 17:05:51 +0900

    FEAT: TaskService 정렬

--- src/main/java/org/example/dotoli/service/TaskService.java
+++ src/main/java/org/example/dotoli/service/TaskService.java
@@ -24,78 +24,78 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class TaskService {
 
-	private final TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
-	private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-	/**
-	 * 할 일 추가
-	 */
-	@Transactional
-	public Long saveTask(TaskRequestDto dto, Long currentMemberId) {
-		Member member = memberRepository.getReferenceById(currentMemberId);
-		Task task = Task.createPersonalTask(dto.getContent(), member);
+    /**
+     * 할 일 추가
+     */
+    @Transactional
+    public Long saveTask(TaskRequestDto dto, Long currentMemberId) {
+        Member member = memberRepository.getReferenceById(currentMemberId);
+        Task task = Task.createPersonalTask(dto.getContent(), member);
 
-		return taskRepository.save(task).getId();
-	}
+        return taskRepository.save(task).getId();
+    }
 
-	/**
-	 * 할 일 목록 조회
-	 */
-	public List<TaskResponseDto> findAll(Long currentMemberId) {
-		return taskRepository.findAllSorted(currentMemberId).stream()
-				.map(task -> new TaskResponseDto(task.getId(), task.getContent(), task.isDone()))
-				.toList();
-	}
+    /**
+     * 할 일 목록 조회
+     */
+    public List<TaskResponseDto> findAll(Long currentMemberId) {
+        return taskRepository.findAllSorted(currentMemberId).stream()
+                .map(task -> new TaskResponseDto(task.getId(), task.getContent(), task.isDone()))
+                .toList();
+    }
 
-	/**
-	 * 할 일 수정
-	 */
-	@Transactional
-	public void updateTask(Long targetId, TaskRequestDto dto, Long currentMemberId) {
-		Task task = findTaskAndValidateOwnership(targetId, currentMemberId);
+    /**
+     * 할 일 수정
+     */
+    @Transactional
+    public void updateTask(Long targetId, TaskRequestDto dto, Long currentMemberId) {
+        Task task = findTaskAndValidateOwnership(targetId, currentMemberId);
 
-		task.updateContent(dto.getContent());
-	}
+        task.updateContent(dto.getContent());
+    }
 
-	/**
-	 * 할 일 삭제
-	 */
-	@Transactional
-	public void deleteTask(Long targetId, Long currentMemberId) {
-		Task task = findTaskAndValidateOwnership(targetId, currentMemberId);
+    /**
+     * 할 일 삭제
+     */
+    @Transactional
+    public void deleteTask(Long targetId, Long currentMemberId) {
+        Task task = findTaskAndValidateOwnership(targetId, currentMemberId);
 
-		taskRepository.delete(task);
-	}
+        taskRepository.delete(task);
+    }
 
-	/**
-	 * 할 일 완료 상태 변경
-	 */
-	@Transactional
-	public void toggleDone(Long targetId, ToggleRequestDto dto, Long currentMemberId) {
-		Task task = findTaskAndValidateOwnership(targetId, currentMemberId);
+    /**
+     * 할 일 완료 상태 변경
+     */
+    @Transactional
+    public void toggleDone(Long targetId, ToggleRequestDto dto, Long currentMemberId) {
+        Task task = findTaskAndValidateOwnership(targetId, currentMemberId);
 
-		task.updateDone(dto.isDone());
-	}
+        task.updateDone(dto.isDone());
+    }
 
-	private Task findTaskAndValidateOwnership(Long taskId, Long currentMemberId) {
-		Task task = taskRepository.findById(taskId)
-				.orElseThrow(TaskNotFoundException::new);
+    private Task findTaskAndValidateOwnership(Long taskId, Long currentMemberId) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(TaskNotFoundException::new);
 
-		validateTaskOwnership(task.getMember().getId(), currentMemberId);
-		
-		return task;
-	}
+        validateTaskOwnership(task.getMember().getId(), currentMemberId);
 
-	private void validateTaskOwnership(Long taskOwnerId, Long currentMemberId) {
-		if (!taskOwnerId.equals(currentMemberId)) {
-			throw new ForbiddenException("해당 항목을 수정할 권한이 없습니다.");
-		}
-	}
+        return task;
+    }
 
-	@Transactional(readOnly = true)
-	public Long getTotalTaskCountForMember(Long memberId){
-		return taskRepository.countAllTasksByMemberId(memberId);
-	}
+    private void validateTaskOwnership(Long taskOwnerId, Long currentMemberId) {
+        if (!taskOwnerId.equals(currentMemberId)) {
+            throw new ForbiddenException("해당 항목을 수정할 권한이 없습니다.");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Long getTotalTaskCountForMember(Long memberId) {
+        return taskRepository.countAllTasksByMemberId(memberId);
+    }
 
 }
