0b89f623fc9b2b9bd982970a82900676fadc4af2
liimjiin <limiary27@gmail.com>
2024-10-11 17:07:17 +0900

    FEAT: MyPageResponseDto 변수 및 생성자 작성

--- src/main/java/org/example/dotoli/dto/member/MyPageResponseDto.java
+++ src/main/java/org/example/dotoli/dto/member/MyPageResponseDto.java
@@ -1,5 +1,31 @@
 package org.example.dotoli.dto.member;
 
+import lombok.Data;
+
+/**
+ * MyPage 정보를 담는 DTO 클래스
+ */
+@Data
 public class MyPageResponseDto {
+
+    private String email;
+
+    private String nickname;
+
+    private int totalTaskCount;
+
+    private int completedTaskCount;
+
+    private int achievementRate;
+
+    public MyPageResponseDto(
+            String email, String nickname, int totalTaskCount, int completedTaskCount, int achievementRate
+    ) {
+        this.email = email;
+        this.nickname = nickname;
+        this.totalTaskCount = totalTaskCount;
+        this.completedTaskCount = completedTaskCount;
+        this.achievementRate = achievementRate;
+    }
 
 }
