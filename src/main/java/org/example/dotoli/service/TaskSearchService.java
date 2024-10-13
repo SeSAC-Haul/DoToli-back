b3ad4b526cb9ff9eba45b8491bba432e535e37ec
jjong <whdgh9588@gmail.com>
2024-10-14 04:55:19 +0900

    FEAT: TaskSearchService에 페이징 기능 추가
    - Task 검색 시 페이징을 적용하기 위해 `searchTasksByContentOrTeam` 메서드를 수정했습니다.

--- src/main/java/org/example/dotoli/service/TaskSearchService.java
+++ src/main/java/org/example/dotoli/service/TaskSearchService.java
@@ -9,6 +9,10 @@ import org.example.dotoli.domain.QTask;
 import org.example.dotoli.domain.QTeam;
 import org.example.dotoli.domain.QTeamMember;
 import org.example.dotoli.domain.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,10 +24,12 @@ public class TaskSearchService {
 
 	private final JPAQueryFactory queryFactory;
 
-	public List<TaskResponseDto> searchTasksByContentOrTeam(Long memberId, String keyword) {
+	public Page<TaskResponseDto> searchTasksByContentOrTeam(Long memberId, String keyword, int page) {
 		QTask task = QTask.task;
 		QTeam team = QTeam.team;
 		QTeamMember teamMember = QTeamMember.teamMember;
+
+		Pageable pageable = PageRequest.of(page, 5);
 
 		/**
 		 * content로 검색하거나, 팀 이름으로 검색 (현재 사용자 또는 같은 팀에 한정)
@@ -41,10 +47,14 @@ public class TaskSearchService {
 				)
 				.fetch();
 
-		return tasks.stream()
+		int start = (int)pageable.getOffset();
+		int end = Math.min((start + pageable.getPageSize()), tasks.size());
+		List<TaskResponseDto> taskDtos = tasks.subList(start, end).stream()
 				.map(taskEntity -> new TaskResponseDto(taskEntity.getId(), taskEntity.getContent(),
 						taskEntity.isDone()))
 				.collect(Collectors.toList());
+
+		return new PageImpl<>(taskDtos, pageable, tasks.size());
 	}
 
 }
