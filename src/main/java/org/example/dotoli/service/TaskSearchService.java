8cc18f8e816580e7a6b94f5814e48875749cd8dc
jjong <whdgh9588@gmail.com>
2024-10-14 05:11:15 +0900

    STYLE: import 구문 정렬 및 코드 포맷 정리, COMMENT: TaskSearchService 주석 제거

--- src/main/java/org/example/dotoli/service/TaskSearchService.java
+++ src/main/java/org/example/dotoli/service/TaskSearchService.java
@@ -1,22 +1,22 @@
 package org.example.dotoli.service;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import lombok.RequiredArgsConstructor;
-
-import org.example.dotoli.dto.task.TaskResponseDto;
 import org.example.dotoli.domain.QTask;
 import org.example.dotoli.domain.QTeam;
 import org.example.dotoli.domain.QTeamMember;
 import org.example.dotoli.domain.Task;
+import org.example.dotoli.dto.task.TaskResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
