2be713e545a78d298743be145e2f1149c64805f4
Chan-GN <ocg0229@gmail.com>
2024-10-17 19:41:40 +0900

    REFACTOR: TeamRequestDto @Builder 어노테이션 제거
    - 사용되지 않는 @Builder 어노테이션 제거

--- src/main/java/org/example/dotoli/dto/task/TaskRequestDto.java
+++ src/main/java/org/example/dotoli/dto/task/TaskRequestDto.java
@@ -4,14 +4,12 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 import lombok.Data;
 
 /**
  * Task 항목 생성/수정 요청 정보를 담는 DTO 클래스
  */
 @Data
-@Builder
 public class TaskRequestDto {
 
 	@NotBlank(message = "내용은 필수 입력값입니다.")
