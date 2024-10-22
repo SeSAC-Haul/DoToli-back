32cff06bb52ee043a086872e695c4d3437d7006d
liimjiin <limiary27@gmail.com>
2024-10-23 00:48:22 +0900

    FEAT: EmailTokenRepository 삭제 기능 추가 - 특정 이메일에 해당하는 EmailToken 엔티티를 삭제하는 메서드 추가

--- src/main/java/org/example/dotoli/repository/EmailTokenRepository.java
+++ src/main/java/org/example/dotoli/repository/EmailTokenRepository.java
@@ -1,14 +1,25 @@
 package org.example.dotoli.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.example.dotoli.domain.EmailToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.transaction.Transactional;
 
 public interface EmailTokenRepository extends JpaRepository<EmailToken, Long> {
 
-	Optional<Object> findByEmail(String email);
+	List<EmailToken> findAllByEmail(String email);
 
 	Optional<Object> findByToken(String token);
+
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM EmailToken e WHERE e.email = :email")
+	void deleteByEmail(@Param("email") String email);
 
 }
